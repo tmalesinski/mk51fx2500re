727f0c2ae1c574efeef7f999b37f018301e664ee
Tomek Malesiński <tmalesinski@gmail.com>
2025-08-03 23:11:51 +0200

    Prepare to support multiple images

--- read_rom.py
+++ read_rom.py
@@ -2,6 +2,9 @@ import imageio
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.ndimage
+import typing
+
+from dataclasses import dataclass
 
 def load_image():
     return imageio.imread("img/mk51_rom_die.jpg")
@@ -9,6 +12,29 @@ def load_image():
 NROWS = 16 * 5
 NCOLS = 16 * 22
 
+@dataclass
+class ImageDesc:
+    path: str
+    width: int
+    height: int
+    rows_corners: typing.Any  # TODO: more accurate type?
+    cols_corners: typing.Any
+
+@dataclass
+class Image:
+    desc: ImageDesc
+    img: np.ndarray
+    gray: np.ndarray
+
+MK51_ROM = ImageDesc(
+    path = "img/mk51_rom_die.jpg",
+    width = 4096,
+    height = 1396,
+    rows_corners = [[(510, 197.5), (494.6, 1108.5)],
+                    [(3743.1, 202.0), (0, 0)]],
+    cols_corners = [[(534.7, 0), (3712.7, 0)], [(0, 0), (0, 0)]])
+
+# TODO: delete
 R00 = (510, 197.5)
 R10 = (3743.1, 202.0)
 R01 = (494.6, 1108.5)
@@ -16,34 +42,43 @@ R01 = (494.6, 1108.5)
 CX0 = 534.7
 CX1 = 3712.7
 
-def bit_pos(i, j):
-    x = CX0 + (CX1 - CX0) * j / (16 * 22 - 1)
-    y = (R00[1] + (R01[1] - R00[1]) * i / (16 * 5 - 1) +
-         (x - R00[0]) / (R10[0] - R00[0]) * (R10[1] - R00[1]))
+def load_image(desc):
+    img = imageio.imread(desc.path)
+    gray = np.mean(img, axis=-1)
+    return Image(desc, img, gray)
+
+def bit_pos(desc, i, j):
+    cx0 = desc.cols_corners[0][0][0]
+    cx1 = desc.cols_corners[0][1][0]
+    r00 = desc.rows_corners[0][0]
+    r01 = desc.rows_corners[0][1]
+    r10 = desc.rows_corners[1][0]
+    x = cx0 + (cx1 - cx0) * j / (16 * 22 - 1)
+    y = (r00[1] + (r01[1] - r00[1]) * i / (16 * 5 - 1) +
+         (x - r00[0]) / (r10[0] - r00[0]) * (r10[1] - r00[1]))
     return (x, y)
 
 
-def get_area(gray, i, j, r, norm=False):
+def get_area(image, i, j, r, norm=False):
     if norm:
-        neighb = get_area(gray, i, j, 10 * r, norm=False)
-        area = get_area(gray, i, j, r, norm=False)
+        neighb = get_area(image, i, j, 10 * r, norm=False)
+        area = get_area(image, i, j, r, norm=False)
         return (area - np.mean(neighb)) / np.std(neighb)
 
-    x, y = bit_pos(i, j)
+    x, y = bit_pos(image.desc, i, j)
     res = scipy.ndimage.affine_transform(
-        gray, [1, 1], offset=(y - r, x - r), output_shape=(2 * r, 2 * r),
+        image.gray, [1, 1], offset=(y - r, x - r), output_shape=(2 * r, 2 * r),
         prefilter=False)
     return res
 
 
-def get_random_bits(img):
-    gray = np.mean(img, axis=-1)
+def get_random_bits(image):
     n = 500
     rows = np.random.randint(0, NROWS, n)
     cols = np.random.randint(0, NCOLS, n)
     res = []
     for r, c in zip(rows, cols):
-        res.append(get_area(gray, r, c, 3).flatten())
+        res.append(get_area(image, r, c, 3).flatten())
     return np.array(res)
 
 
@@ -54,9 +89,8 @@ def pca_bits(bits):
     return v[0], v[1]
 
 
-def get_pc(img):
-    gray = np.mean(img, axis=-1)
-    rbits = get_random_bits(img)
+def get_pc(image):
+    rbits = get_random_bits(image)
     plt.imshow(rbits); plt.show()
     c1, c2 = pca_bits(rbits)
     m = np.mean(rbits, axis=0)
@@ -83,15 +117,14 @@ C1 = np.array([-0.04704893, -0.09579545, -0.22798194, -0.28930118, -0.18566232,
               0.10222208])
 THR = 27
 
-def read_bits(img):
-    gray = np.mean(img, axis=-1)
+def read_bits(image):
     res = np.zeros((NROWS, NCOLS), dtype=bool)
     for i in range(NROWS):
         for j in range(NCOLS):
-            ar = get_area(gray, i, j, 3).flatten()
+            ar = get_area(image, i, j, 3).flatten()
             # TODO: classify 0/1 based on one example
-            res[i, j] = np.dot(ar - M, C1) > THR
-    return res
+            res[i, j] = np.dot(ar - M, C1) < THR
+    return res.astype(int)
 
 def dist_from_means(bits, m):
     d = []
@@ -109,17 +142,17 @@ def kmeans(bits, m0, m1):
             m.append(np.mean(bits[closer == i], axis=0))
     return tuple(m)
 
-def read_with_kmeans_on_rows(gray, start, limit):
+def read_with_kmeans_on_rows(image, start, limit):
     norm = False
     bits = []
     nr = limit - start
     nc = NCOLS
     for i in range(start, limit):
         for j in range(NCOLS):
-            bits.append(get_area(gray, i, j, 3, norm=norm).flatten())
+            bits.append(get_area(image, i, j, 3, norm=norm).flatten())
     bits = np.array(bits)
-    ex1 = get_area(gray, 1, 1, 3, norm=norm).flatten()
-    ex0 = get_area(gray, 2, 1, 3, norm=norm).flatten()
+    ex1 = get_area(image, 1, 1, 3, norm=norm).flatten()
+    ex0 = get_area(image, 2, 1, 3, norm=norm).flatten()
     m = kmeans(bits, ex0, ex1)
     d = dist_from_means(bits, m)
 
@@ -136,16 +169,15 @@ def read_with_kmeans_on_rows(gray, start, limit):
     return np.argmin(d, axis=0).reshape(nr, nc), ind
 
 
-def read_with_kmeans(img):
+def read_with_kmeans(image):
     # TODO: check how good this is, maybe try with normalization
     # gray = scipy.ndimage.gaussian_filter(scipy.ndimage.laplace(np.mean(img, axis=-1)), 2)
-    gray = np.mean(img, axis=-1)
     parts = []
     outliers = ([], [])
     for i in range(2):
         start = NROWS * i // 2
         limit = NROWS * (i + 1) // 2
-        v, outl = read_with_kmeans_on_rows(gray, start, limit)
+        v, outl = read_with_kmeans_on_rows(image, start, limit)
         parts.append(v)
         outliers[0].extend(np.array(outl[0]) + start)
         outliers[1].extend(outl[1])
@@ -159,21 +191,21 @@ def dump_str(read_bits):
             rows.append("".join([str(b) for b in read_bits[i * 5 + j]]))
     return "\n".join(rows)
 
-def show_bits(img, bits, v):
-    plt.imshow(img)
+def show_bits(image, bits, v):
+    plt.imshow(image.img)
     points = []
     for i in range(NROWS):
         for j in range(NCOLS):
             if bits[i, j] == v:
-                points.append(bit_pos(i, j))
+                points.append(bit_pos(image.desc, i, j))
     plt.plot([p[0] for p in points], [p[1] for p in points], 'o', alpha=0.3)
     plt.show()
 
-def show_outliers(img, bits, outliers, v):
-    plt.imshow(img)
+def show_outliers(image, bits, outliers, v):
+    plt.imshow(image.img)
     points = []
     for i, j in zip(*outliers):
         if bits[i, j] == v:
-            points.append(bit_pos(i, j))
+            points.append(bit_pos(image.desc, i, j))
     plt.plot([p[0] for p in points], [p[1] for p in points], 'o', alpha=0.3)
     plt.show()
